b09098d9379a6b4641cf9d2f66183fc6f4eac974
ofu997 <ofu997@gmail.com>
2019-07-09 15:57:02 -0700

    move starter comments down

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,23 +6,13 @@ int min = 1, max = 5;
 Random rndm = new Random();
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demonstration',
       theme: ThemeData(
-        // This is the theme of your application.
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.teal,
         primaryColor: Colors.tealAccent[700],
-
       ),
       home: MyHomePage('Game'), // change from Flutter Demo Home Page
     );
@@ -34,20 +23,6 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   MyHomePage(this.title);
-
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State objerct (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  // void initState(
-
-  // )
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -68,56 +43,24 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
-
   void _next() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
       questionNumber++;
       random = min + rndm.nextInt(max-min); // Between 0 
     });
   }
 
-  
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title, 
           style: TextStyle(color: Colors.orange[300]),
         ),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -137,7 +80,59 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _next,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
+
+// This trailing comma makes auto-formatting nicer for build methods.
+// Column is also layout widget. It takes a list of children and
+// Center is a layout widget. It takes a single child and positions it
+// Here we take the value from the MyHomePage object that was created by
+// This method is rerun every time setState is called, for instance as done
+// This call to setState tells the Flutter framework that something has
+// This widget is the home page of your application. It is stateful, meaning
+// This is the theme of your application.
+// This widget is the root of your application.
+// Try running your application with "flutter run". You'll see the
+// application has a blue toolbar. Then, without quitting the app, try
+// changing the primarySwatch below to Colors.green and then invoke
+// "hot reload" (press "r" in the console where you ran "flutter run",
+// or simply save your changes to "hot reload" in a Flutter IDE).
+// Notice that the counter didn't reset back to zero; the application
+// is not restarted.
+// that it has a State objerct (defined below) that contains fields that affect
+// how it looks.
+
+// This class is the configuration for the state. It holds the values (in this
+// case the title) provided by the parent (in this case the App widget) and
+// used by the build method of the State. Fields in a Widget subclass are
+// always marked "final".
+
+// void initState(
+
+// )
+// changed in this State, which causes it to rerun the build method below
+// so that the display can reflect the updated values. If we changed
+// _counter without calling setState(), then the build method would not be
+// called again, and so nothing would appear to happen.
+// by the _incrementCounter method above.
+//
+// The Flutter framework has been optimized to make rerunning build methods
+// fast, so that you can just rebuild anything that needs updating rather
+// than having to individually change instances of widgets.
+// the App.build method, and use it to set our appbar title.
+// in the middle of the parent.
+// arranges them vertically. By default, it sizes itself to fit its
+// children horizontally, and tries to be as tall as its parent.
+//
+// Invoke "debug painting" (press "p" in the console, choose the
+// "Toggle Debug Paint" action from the Flutter Inspector in Android
+// Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+// to see the wireframe for each widget.
+//
+// Column has various properties to control how it sizes itself and
+// how it positions its children. Here we use mainAxisAlignment to
+// center the children vertically; the main axis here is the vertical
+// axis because Columns are vertical (the cross axis would be
+// horizontal).
